3cd0bd535b66edeffe959df9b11c07b20ce41d66
Fubuki <klick4278@gmail.com>
2023-08-28 00:03:48 +0530

    (web/indexTS) - added

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+<meta name="viewport" content="initial-scale=1, width=device-width" />
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
