dd93527bebacc32816a2f4d51fbba013cb9e70eb
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-08-31 12:25:39 +0530

    experiment http

---
+++ server/http.js
@@ -0,0 +1,25 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// function handleFirstRequest(req, res) {
+//   var calculatedSum = CalculateSum(100);
+//   console.log(calculatedSum);
+//   var ans = "The calculated sum = " + calculatedSum;
+//   res.send(ans)
+// }
+
+// app.get('/', handleFirstRequest)
+
+// function started() { 
+//   console.log(`Example app listening on port ${port}`)
+// }
+
+// app.listen(port, started)
+
+// function CalculateSum(counter) {
+//   var sum = 0 ;
+//   for(var i=0; i<counter; i++) {
+//     sum += i;
+//   }
+//   return sum ;
