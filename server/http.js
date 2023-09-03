bafaef38d2023e3bd339c020a39a698cfee7b294
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-03 22:38:22 +0530

    added modules
    added new extension for backend

--- server/http.js
+++ server/http.js
@@ -1,26 +1,52 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
+// Import required modules
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
-// function handleFirstRequest(req, res) {
-//   var calculatedSum = CalculateSum(100);
-//   console.log(calculatedSum);
-//   var ans = "The calculated sum = " + calculatedSum;
-//   res.send(ans)
-// }
+// Create an Express application
+const app = express();
 
-// app.get('/', handleFirstRequest)
+// Connect to MongoDB (Make sure MongoDB is running)
+mongoose.connect('mongodb://localhost:27017/User', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// function started() { 
-//   console.log(`Example app listening on port ${port}`)
-// }
+// Define a User schema
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
-// app.listen(port, started)
+const User = mongoose.model('User', UserSchema);
 
-// function CalculateSum(counter) {
-//   var sum = 0 ;
-//   for(var i=0; i<counter; i++) {
-//     sum += i;
-//   }
-//   return sum ;
+// Middleware to parse JSON in requests
+app.use(bodyParser.json());
 
+// POST route to handle user sign-in
+app.post('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the user exists in the database
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  // Compare the provided password with the stored hashed password
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    return res.status(401).json({ message: 'Incorrect password' });
+  }
+
+  // If the password matches, you can consider the user authenticated.
+  // You may generate a JWT token and return it here for further authentication.
+
+  res.status(200).json({ message: 'Sign-in successful' });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
