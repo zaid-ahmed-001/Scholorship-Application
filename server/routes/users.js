5eb5c79f231926f460392592bcdc67c27100c9de
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-08 16:01:06 +0530

    server/routes/user.js
    created some CURD methods through RESTfull apis

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,4 +12,19 @@ router.get('/:id',(req, res) => {
     res.json({mssg: 'GET a single user'})
 } )
 
+//post a user
+router.post('/',(req, res)  => {
+    res.json({mssg:'POST a new user details'})
+})
+
+//delete a user
+router.delete('/:id',(req, res)  => {
+    res.json({mssg:'DELETE a user details'})
+})
+
+//update a user detail
+router.patch('/:id',(req, res)  => {
+    res.json({mssg:'UPDATE a user details'})
+})
+
 module.exports= router
