603838865087c48a7a1f5b021d9898320fafbc94
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-08 14:36:19 +0530

    server/routes/users.js
    updated get requests

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/',() => {} )
+//get all users 
+router.get('/',(req, res) => {
+    res.json({mssg: 'GET all user'})
+} )
+
+//get a single user
+router.get('/:id',(req, res) => {
+    res.json({mssg: 'GET a single user'})
+} )
 
 module.exports= router
