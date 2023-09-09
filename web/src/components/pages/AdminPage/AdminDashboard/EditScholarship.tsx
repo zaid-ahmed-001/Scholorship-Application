02d0018530490b224129c37fdc976ea77f1aa735
Fubuki <klick4278@gmail.com>
2023-09-09 17:53:25 +0530

    (web/EditScholarship) - added

---
+++ web/src/components/pages/AdminPage/AdminDashboard/EditScholarship.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditScholarship = () => {
+    return (
+        <>
+        <h1>EditScholarship</h1>
+        </>
+    );
+};
+
+export default EditScholarship;
