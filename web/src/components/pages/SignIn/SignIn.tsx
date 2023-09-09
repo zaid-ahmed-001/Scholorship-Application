2cdc6c3c97132874c299b64fccf13235a84da95b
Fubuki <klick4278@gmail.com>
2023-09-09 17:51:40 +0530

    (web/SignIn) - added

---
+++ web/src/components/pages/SignIn/SignIn.tsx
@@ -0,0 +1,214 @@
+import * as React from 'react';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+import GlobalStyles from '@mui/joy/GlobalStyles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Checkbox from '@mui/joy/Checkbox';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel, { formLabelClasses } from '@mui/joy/FormLabel';
+import Link from '@mui/joy/Link';
+import Input from '@mui/joy/Input';
+import Typography from '@mui/joy/Typography';
+import { useNavigate } from "react-router-dom";
+import ColorSchemeToggle from '../../utils/ColorSchemeToggle';
+interface FormElements extends HTMLFormControlsCollection {
+  text: HTMLInputElement;
+  password: HTMLInputElement;
+  persistent: HTMLInputElement;
+}
+interface SignInFormElement extends HTMLFormElement {
+  readonly elements: FormElements;
+}
+
+export default function SignIn() {
+  React.useEffect(() => {
+    document.title = "SignIn - EduFundr";
+    return () => {};
+  }, []);
+  const navigate = useNavigate();
+  function handleClickToSignUp() {
+    navigate("/SignUp");
+   
+  }
+    function handleClickTogeneral() {
+      navigate("/general");
+     
+    }
+  return (
+    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ':root': {
+            '--Collapsed-breakpoint': '769px', // form will stretch when viewport is below `769px`
+            '--Cover-width': '40vw', // must be `vw` only
+            '--Form-maxWidth': '700px',
+            '--Transition-duration': '0.4s', // set to `none` to disable transition
+          },
+        }}
+      />
+      <Box
+        sx={(theme) => ({
+          width:
+            'clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)',
+          transition: 'width var(--Transition-duration)',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          position: 'relative',
+          zIndex: 1,
+          display: 'flex',
+          justifyContent: 'flex-end',
+          backdropFilter: 'blur(4px)',
+          backgroundColor: 'rgba(255 255 255 / 0.6)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundColor: 'rgba(19 19 24 / 0.4)',
+          },
+        })}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100dvh',
+            width:
+              'clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)',
+            maxWidth: '100%',
+            px: 2,
+          }}
+        >
+          <Box
+            component="header"
+            sx={{
+              py: 3,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography
+              fontWeight="lg"
+              startDecorator={
+                <Box
+                  component="span"
+                  sx={{
+                    width: 24,
+                    height: 24,
+                    background: (theme) =>
+                      `linear-gradient(45deg, ${theme.vars.palette.primary.solidBg}, ${theme.vars.palette.primary.solidBg} 30%, ${theme.vars.palette.primary.softBg})`,
+                    borderRadius: '50%',
+                    opacity: 0,
+                    boxShadow: (theme) => theme.shadow.md,
+                    '--joy-shadowChannel': (theme) =>
+                      theme.vars.palette.primary.mainChannel,
+                  }}
+                />
+              }
+            >
+            </Typography>
+            <ColorSchemeToggle />
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              my: 'auto',
+              py: 2,
+              pb: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              width: 400,
+              maxWidth: '100%',
+              mx: 'auto',
+              borderRadius: 'sm',
+              '& form': {
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+              },
+              [`& .${formLabelClasses.asterisk}`]: {
+                visibility: 'hidden',
+              },
+            }}
+          >
+            <div>
+              <Typography component="h1" fontSize="xl2" fontWeight="lg">
+                Sign in to your account
+              </Typography>
+              <Typography level="body-sm" sx={{ my: 1, mb: 3 }}>
+                Welcome back
+              </Typography>
+            </div>
+            <form
+              onSubmit={(event: React.FormEvent<SignInFormElement>) => {
+                event.preventDefault();
+                const formElements = event.currentTarget.elements;
+                const data = {
+                  text: formElements.text.value,
+                  password: formElements.password.value,
+                  persistent: formElements.persistent.checked,
+                };
+                handleClickTogeneral();
+                alert(JSON.stringify(data, null, 2));
+              }}
+            >
+              <FormControl required>
+                <FormLabel>UserID</FormLabel>
+                <Input type="text" name="text" />
+              </FormControl>
+              <FormControl required>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" name="password" />
+              </FormControl>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}
+              >
+                <Checkbox size="sm" label="Remember me" name="persistent" />
+                <Link onClick={()=>navigate('/ForgotPassword')}fontWeight="lg">
+                  Forgot your password?
+                </Link>
+              </Box>
+              <Button type="submit"  fullWidth>
+                Sign in
+              </Button>
+               <Link fontSize="sm" onClick={handleClickToSignUp} fontWeight="lg">
+                Newcomer? Sign Up Now!
+                </Link>
+            </form>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" textAlign="center">
+              © EduFundr By Zaid Ahmed & Shashank Mishra {new Date().getFullYear()}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={(theme) => ({
+          height: '100%',
+          position: 'fixed',
+          right: 0,
+          top: 0,
+          bottom: 0,
+          left: 'clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))',
+          transition:
+            'background-image var(--Transition-duration), left var(--Transition-duration) !important',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          backgroundColor: 'background.level1',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundImage:
+            'url(https://cdn.discordapp.com/attachments/1144894054505664612/1144895631958229022/sidePickLightMode.png?auto=format&w=1000&dpr=2)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundImage:
+              'url(https://cdn.discordapp.com/attachments/1144894054505664612/1144896167289819207/sidePickDarkMode.png?auto=format&w=1000&dpr=2)',
+          },
+        })}
+      />
+    </CssVarsProvider>
+  );
+}
