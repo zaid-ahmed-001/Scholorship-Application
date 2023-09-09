3f41df2d7d918e853348463a27b8e1b897408dc6
Fubuki <klick4278@gmail.com>
2023-09-09 17:49:33 +0530

    (web/AppliedScholarshipTable) - added

---
+++ web/src/components/pages/UserPage/ScholarshipProfile/elements/AppliedScholarshipTable.tsx
@@ -0,0 +1,425 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import * as React from 'react';
+import { ColorPaletteProp } from '@mui/joy/styles';
+import Avatar from '@mui/joy/Avatar';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Chip from '@mui/joy/Chip';
+import Divider from '@mui/joy/Divider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Link from '@mui/joy/Link';
+import Input from '@mui/joy/Input';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import ModalClose from '@mui/joy/ModalClose';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import Table from '@mui/joy/Table';
+import Sheet from '@mui/joy/Sheet';
+import Checkbox from '@mui/joy/Checkbox';
+import IconButton, { iconButtonClasses } from '@mui/joy/IconButton';
+import Typography from '@mui/joy/Typography';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+import Dropdown from '@mui/joy/Dropdown';
+// icons
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import BlockIcon from '@mui/icons-material/Block';
+import AutorenewRoundedIcon from '@mui/icons-material/AutorenewRounded';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+
+const rows = [
+  {
+    id: 'INV-1234',
+    date: 'Feb 3, 2023',
+    status: 'Refunded',
+    customer: {
+      initial: 'O',
+      name: 'Olivia Ryhe',
+      email: 'olivia@email.com',
+    },
+  },
+  {
+    id: 'INV-1233',
+    date: 'Feb 3, 2023',
+    status: 'Paid',
+    customer: {
+      initial: 'S',
+      name: 'Steve Hampton',
+      email: 'steve.hamp@email.com',
+    },
+  },
+  {
+    id: 'INV-1232',
+    date: 'Feb 3, 2023',
+    status: 'Refunded',
+    customer: {
+      initial: 'C',
+      name: 'Ciaran Murray',
+      email: 'ciaran.murray@email.com',
+    },
+  },
+  {
+    id: 'INV-1231',
+    date: 'Feb 3, 2023',
+    status: 'Refunded',
+    customer: {
+      initial: 'M',
+      name: 'Maria Macdonald',
+      email: 'maria.mc@email.com',
+    },
+  },
+  {
+    id: 'INV-1230',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'C',
+      name: 'Charles Fulton',
+      email: 'fulton@email.com',
+    },
+  },
+  {
+    id: 'INV-1229',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'J',
+      name: 'Jay Hooper',
+      email: 'hooper@email.com',
+    },
+  },
+  {
+    id: 'INV-1228',
+    date: 'Feb 3, 2023',
+    status: 'Refunded',
+    customer: {
+      initial: 'K',
+      name: 'Krystal Stevens',
+      email: 'k.stevens@email.com',
+    },
+  },
+  {
+    id: 'INV-1227',
+    date: 'Feb 3, 2023',
+    status: 'Paid',
+    customer: {
+      initial: 'S',
+      name: 'Sachin Flynn',
+      email: 's.flyn@email.com',
+    },
+  },
+  {
+    id: 'INV-1226',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'B',
+      name: 'Bradley Rosales',
+      email: 'brad123@email.com',
+    },
+  },
+  {
+    id: 'INV-1234',
+    date: 'Feb 3, 2023',
+    status: 'Paid',
+    customer: {
+      initial: 'O',
+      name: 'Olivia Ryhe',
+      email: 'olivia@email.com',
+    },
+  },
+  {
+    id: 'INV-1233',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'S',
+      name: 'Steve Hampton',
+      email: 'steve.hamp@email.com',
+    },
+  },
+  {
+    id: 'INV-1232',
+    date: 'Feb 3, 2023',
+    status: 'Paid',
+    customer: {
+      initial: 'C',
+      name: 'Ciaran Murray',
+      email: 'ciaran.murray@email.com',
+    },
+  },
+  {
+    id: 'INV-1231',
+    date: 'Feb 3, 2023',
+    status: 'Refunded',
+    customer: {
+      initial: 'M',
+      name: 'Maria Macdonald',
+      email: 'maria.mc@email.com',
+    },
+  },
+  {
+    id: 'INV-1230',
+    date: 'Feb 3, 2023',
+    status: 'Paid',
+    customer: {
+      initial: 'C',
+      name: 'Charles Fulton',
+      email: 'fulton@email.com',
+    },
+  },
+  {
+    id: 'INV-1229',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'J',
+      name: 'Jay Hooper',
+      email: 'hooper@email.com',
+    },
+  },
+  {
+    id: 'INV-1228',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'K',
+      name: 'Krystal Stevens',
+      email: 'k.stevens@email.com',
+    },
+  },
+  {
+    id: 'INV-1227',
+    date: 'Feb 3, 2023',
+    status: 'Paid',
+    customer: {
+      initial: 'S',
+      name: 'Sachin Flynn',
+      email: 's.flyn@email.com',
+    },
+  },
+  {
+    id: 'INV-1226',
+    date: 'Feb 3, 2023',
+    status: 'Cancelled',
+    customer: {
+      initial: 'B',
+      name: 'Bradley Rosales',
+      email: 'brad123@email.com',
+    },
+  },
+];
+
+export default function AppliedScholarshipTable() {
+
+  return (
+    <React.Fragment>
+      <Box
+      component="main"
+      className="MainContent"
+      sx={{
+          px: {
+          xs: 2,
+          md: 4,
+          },
+          pt: {
+          xs: 'calc(12px + var(--Header-height))',
+          sm: 'calc(12px + var(--Header-height))',
+          md: 3,
+          },
+          pb: {
+          xs: 2,
+          sm: 2,
+          md: 3,
+          },
+          flex: 1,          
+          display: {xs: 'none', sm:  'flex', md: 'flex', lg:'flex'},
+          flexDirection: 'column',
+          minWidth: 0,
+          height: '100dvh',
+          gap: 1,
+      }}
+      >
+        <Box sx={{ display: {xs: 'none', md: 'flex', lg: 'flex'}, alignItems: 'center' }}>
+            <Breadcrumbs
+            size="sm"
+            aria-label="breadcrumbs"
+            separator={<ChevronRightRoundedIcon />}
+            >
+            <Link
+                underline="none"
+                color="neutral"
+                aria-label="Home"
+            >
+                <HomeRoundedIcon />
+            </Link>
+            <Link
+                underline="hover"
+                color="neutral"
+                fontSize={12}
+                fontWeight={500}
+            >
+                Scholarship Data
+            </Link>
+            <Typography color="primary" fontWeight={500} fontSize={12}>
+                Applied Scholarship
+            </Typography>
+            </Breadcrumbs>
+        </Box>
+        <Sheet
+          className="OrderTableContainer"
+          variant="outlined"
+          sx={{
+            display: { xs: 'none', sm: 'initial' },
+            width: '100%',
+            borderRadius: 'sm',
+            flexShrink: 1,
+            overflow: 'auto',
+            overflowX: 'scroll',
+            minHeight: 0,
+          }}
+        >
+        <Table
+          aria-labelledby="tableTitle"
+          stickyHeader
+          hoverRow
+          sx={{
+            '--TableCell-headBackground': 'var(--joy-palette-background-level1)',
+            '--Table-headerUnderlineThickness': '1px',
+            '--TableRow-hoverBackground': 'var(--joy-palette-background-level1)',
+            '--TableCell-paddingY': '4px',
+            '--TableCell-paddingX': '20px',
+          }}
+        >
+          <thead>
+            <tr>
+              <th style={{ width: 140, padding: '12px 20px'}}>Enrollment Number</th>
+              <th style={{ width: 150, padding: '12px 20px' }}>Scholarship Name</th>
+              <th style={{ width: 100, padding: '12px 20px' }}>Type</th>
+              <th style={{ width: 140, padding: '12px 20px' }}>Apply Date</th>
+              <th style={{ width: 140, padding: '12px 20px' }}>Scholarship Status</th>
+              <th style={{ width: 140, padding: '12px 20px' }}>Payment Status</th>
+              <th style={{ width: 140, padding: '12px 20px' }}>Paid Date</th>
+              <th style={{ width: 90, padding: '12px 20px' }}>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> <Typography level="body-xs">0863CS221189</Typography> </td>
+              <td> <Typography level="body-xs">Chavi Jain Scholarship</Typography> </td>
+              <td> <Typography level="body-xs">Fresh</Typography> </td>
+              <td> <Typography level="body-xs">01 Semptember 2023</Typography> </td>
+              <td>
+                  <Chip
+                    variant="soft"
+                    size="sm"
+                    startDecorator={
+                      {
+                        Paid: <CheckRoundedIcon />,
+                        Refunded: <AutorenewRoundedIcon />,
+                        Cancelled: <BlockIcon />,
+                      }['Paid']
+                    }
+                    color={
+                      {
+                        Paid: 'success',
+                        Refunded: 'neutral',
+                        Cancelled: 'danger',
+                      }['Paid'] as ColorPaletteProp
+                    }
+                  >
+                    Approved
+                  </Chip>
+              </td>
+              <td>
+                  <Chip
+                    variant="soft"
+                    size="sm"
+                    startDecorator={
+                      {
+                        Paid: <CheckRoundedIcon />,
+                        Refunded: <AutorenewRoundedIcon />,
+                        Cancelled: <BlockIcon />,
+                      }['Refunded']
+                    }
+                    color={
+                      {
+                        Paid: 'success',
+                        Refunded: 'neutral',
+                        Cancelled: 'danger',
+                      }['Refunded'] as ColorPaletteProp
+                    }
+                  >
+                  In-Process
+                  </Chip>
+              </td>
+              <td> <Typography level="body-xs"></Typography> </td>
+              <td> <Typography level="body-xs">Rs: 26,000</Typography> </td>
+            </tr>
+            
+            {/* {rows.map((row) => (
+              <tr key={row.id}>
+                <td>
+                  <Typography level="body-xs">{row.id}</Typography>
+                </td>
+                <td>
+                  <Typography level="body-xs">{row.date}</Typography>
+                </td>
+                <td>
+                  <Chip
+                    variant="soft"
+                    size="sm"
+                    startDecorator={
+                      {
+                        Paid: <CheckRoundedIcon />,
+                        Refunded: <AutorenewRoundedIcon />,
+                        Cancelled: <BlockIcon />,
+                      }[row.status]
+                    }
+                    color={
+                      {
+                        Paid: 'success',
+                        Refunded: 'neutral',
+                        Cancelled: 'danger',
+                      }[row.status] as ColorPaletteProp
+                    }
+                  >
+                    {row.status}
+                  </Chip>
+                </td>
+                <td>
+                  <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                    <Avatar size="sm">{row.customer.initial}</Avatar>
+                    <div>
+                      <Typography level="body-xs">{row.customer.name}</Typography>
+                      <Typography level="body-xs">{row.customer.email}</Typography>
+                    </div>
+                  </Box>
+                </td>
+                <td>
+                  <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                    <Link level="body-xs" component="button">
+                      Download
+                    </Link>
+                  </Box>
+                </td>
+              </tr>
+            ))} */}
+          </tbody>
+        </Table>
+      </Sheet>
+      </Box>
+    </React.Fragment>
+  );
+}
