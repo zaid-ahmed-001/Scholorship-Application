c111198927c335c0fca4f2ce7122f02021f7a06b
Fubuki <klick4278@gmail.com>
2023-09-09 17:52:37 +0530

    (web/adminNavbar) - added

---
+++ web/src/components/pages/AdminPage/adminNavbar.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import GlobalStyles from '@mui/joy/GlobalStyles';
+import IconButton from '@mui/joy/IconButton';
+import Sheet from '@mui/joy/Sheet';
+import { toggleSidebar } from './adminSideBar';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
+export default function AdminNavbar() {
+  return (
+    <Sheet
+      sx={{
+        display: { xs: 'flex', md: 'flex', lg: 'none' },
+        alignItems: 'center',
+        position: 'fixed',
+        top: 0,
+        width: '100vw',
+        height: 'var(--Header-height)',
+        zIndex: 9995,
+        py: 1,
+        px: 2,
+        gap: 1,
+        boxShadow: 'sm',
+      }}
+    >
+      <GlobalStyles
+        styles={(theme) => ({
+          ':root': {
+            '--Header-height': '52px',
+            [theme.breakpoints.up('md')]: {
+              '--Header-height': '52px',
+            },
+            [theme.breakpoints.up('lg')]: {
+              '--Header-height': '0px',
+            },
+          },
+        })}
+      />
+      <IconButton
+        onClick={() => toggleSidebar()}
+        variant="outlined"
+        color="neutral"
+        size="sm"
+      >
+        <MenuRoundedIcon />
+      </IconButton>
+      <img src='https://cdn.discordapp.com/attachments/1144894054505664612/1149414171901960242/Logo.png' style={{height: 25}} />
+      {/* <ColorSchemeToggle id={undefined} /> */}
+    </Sheet>
+  );
+}
