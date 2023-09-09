93080f91b9a5b919c4a57eac1e34c64bb7020115
Fubuki <klick4278@gmail.com>
2023-09-09 17:53:40 +0530

    (web/CreateScholarship) - added

---
+++ web/src/components/pages/AdminPage/AdminDashboard/CreateScholarship.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateScholarship = () => {
+    return (
+        <>
+            <h1>CreateScholarship</h1>
+        </>
+    );
+};
+
+export default CreateScholarship;
