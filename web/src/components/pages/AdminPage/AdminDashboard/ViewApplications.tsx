68fc1b033264787ac3bb413bf04b53965f40f55e
Fubuki <klick4278@gmail.com>
2023-09-09 17:52:47 +0530

    (web/ViewApplication) - added

---
+++ web/src/components/pages/AdminPage/AdminDashboard/ViewApplications.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewApplications = () => {
+    return (
+        <>
+            <h1>ViewApplications</h1>
+        </>
+    );
+};
+
+export default ViewApplications;
