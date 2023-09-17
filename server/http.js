6acf73e4fde0368ab06d0c77c4cfdd7020e7b8a7
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-17 20:54:40 +0530

    posted get request

--- server/http.js
+++ server/http.js
@@ -62,6 +62,77 @@ mongoose.connect(process.env.MONGO_URI, {
       }
     });
 
+    // GET route to retrieve a user by ID
+    app.get('/api/users/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+
+        // Check if the provided ID is valid
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          return res.status(404).json({ error: 'Invalid ID' });
+        }
+
+        // Find the user by ID in the database
+        const foundUser = await User.findById(id);
+        if (!foundUser) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(foundUser);
+      } catch (error) {
+        console.error('Error retrieving user by ID:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+    // DELETE route to delete a user by ID
+    app.delete('/api/users/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+
+        // Check if the provided ID is valid
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          return res.status(404).json({ error: 'Invalid ID' });
+        }
+
+        // Find and delete the user by ID in the database
+        const deletedUser = await User.findOneAndDelete({ _id: id });
+        if (!deletedUser) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
+      } catch (error) {
+        console.error('Error deleting user by ID:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+    // PUT route to update a user by ID
+    app.put('/api/users/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+
+        // Check if the provided ID is valid
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+          return res.status(404).json({ error: 'Invalid ID' });
+        }
+
+        // Find and update the user by ID in the database
+        const updatedUser = await User.findOneAndUpdate({ _id: id }, req.body, {
+          new: true,
+        });
+        if (!updatedUser) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+      } catch (error) {
+        console.error('Error updating user by ID:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
     // Start the server once the database connection is established
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
