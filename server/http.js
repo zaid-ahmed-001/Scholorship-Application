3147eaa33a19a5e776127e09d1179bc26492dc48
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-19 02:25:44 +0530

    server/http.js
    user schema was used twice

--- server/http.js
+++ server/http.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -9,146 +11,30 @@ const port = process.env.PORT || 4000;
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
-// Connect to MongoDB Atlas using the provided URI
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-
-    // Define the user schema and model
-    const userSchema = new mongoose.Schema({
-      name: String,
-      mobile: String,
-      Email: String,
-    });
-
-    const User = mongoose.model('User', userSchema);
-
-    // POST route to create a new user
-    app.post('/api/users', async (req, res) => {
-      try {
-        // Destructure properties from req.body
-        const { name, mobile, Email } = req.body;
-
-        // Check if required fields are present
-        if (!name || !mobile || !Email) {
-          return res.status(400).json({ error: 'Missing required fields' });
-        }
-
-        // Create a new user
-        const newUser = new User({ name, mobile, Email });
-
-        // Save the user to the database
-        await newUser.save();
-
-        res.status(201).json({ message: 'User created successfully', user: newUser });
-      } catch (error) {
-        console.error('Error creating user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-
-    // GET route to retrieve all users
-    app.get('/api/users', async (req, res) => {
-      try {
-        // Retrieve all users from the database
-        const users = await User.find();
-        res.status(200).json(users);
-      } catch (error) {
-        console.error('Error retrieving users:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-
-    // GET route to retrieve a user by ID
-    app.get('/api/users/:id', async (req, res) => {
-      try {
-        const { id } = req.params;
-
-        // Check if the provided ID is valid
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-          return res.status(404).json({ error: 'Invalid ID' });
-        }
-
-        // Find the user by ID in the database
-        const foundUser = await User.findById(id);
-        if (!foundUser) {
-          return res.status(404).json({ error: 'User not found' });
-        }
-
-        res.status(200).json(foundUser);
-      } catch (error) {
-        console.error('Error retrieving user by ID:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-
-    // DELETE route to delete a user by ID
-    app.delete('/api/users/:id', async (req, res) => {
-      try {
-        const { id } = req.params;
-
-        // Check if the provided ID is valid
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-          return res.status(404).json({ error: 'Invalid ID' });
-        }
-
-        // Find and delete the user by ID in the database
-        const deletedUser = await User.findOneAndDelete({ _id: id });
-        if (!deletedUser) {
-          return res.status(404).json({ error: 'User not found' });
-        }
-
-        res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
-      } catch (error) {
-        console.error('Error deleting user by ID:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-
-    // PUT route to update a user by ID
-    app.put('/api/users/:id', async (req, res) => {
-      try {
-        const { id } = req.params;
-
-        // Check if the provided ID is valid
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-          return res.status(404).json({ error: 'Invalid ID' });
-        }
-
-        // Find and update the user by ID in the database
-        const updatedUser = await User.findOneAndUpdate({ _id: id }, req.body, {
-          new: true,
-        });
-        if (!updatedUser) {
-          return res.status(404).json({ error: 'User not found' });
-        }
-
-        res.status(200).json({ message: 'User updated successfully', user: updatedUser });
-      } catch (error) {
-        console.error('Error updating user by ID:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-
-    // Start the server once the database connection is established
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-// Handle unhandled promise rejections (for MongoDB connection)
-process.on('unhandledRejection', (error) => {
-  console.error('Unhandled Promise Rejection:', error);
 });
 
-// Handle uncaught exceptions
-process.on('uncaughtException', (error) => {
-  console.error('Uncaught Exception:', error);
-  process.exit(1); // Exit the process to prevent further issues
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Import and use the userRoutes
+const userRoutes = require('./routes/users');
+app.use('/api/users', userRoutes);
+
+// Centralized error handling middleware
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
