8c7de02bc820a179a751cef5b1a1277550c1042c
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-20 03:31:44 +0530

    creates scholarship modal

---
+++ server/models/scholarmodel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const ScholarSchema = new Schema({
+    Name :{
+        type : String,
+        required : true
+    },
+    Deadline :{
+        type : Date,
+        required : true
+    },
+    Description :{
+        type : String,
+        required : true
+    },
+    Eligibility :{
+        type : String,
+        required : true
+    },
+    Doc_req :{
+        type : String,
+        required : true
+    }
+}, { timestamps: true})
+
+module.exports = mongoose.model('Scholarship', ScholarSchema)
