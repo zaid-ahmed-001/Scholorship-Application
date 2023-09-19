d2bc13667152e579f48005e15211431b26144d0d
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-09-20 03:32:28 +0530

    created route

---
+++ server/routes/scholar.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {
+   createScholar,
+   getScholars,
+   getScholar,
+   deleteScholar,
+   updateScholar
+
+} = require('../controllers/scholarcontroller');
+
+const router = express.Router();
+
+// GET all scholar
+router.get('/', getScholars);
+
+// GET a single scholar
+router.get('/:id', getScholar);
+
+// POST a scholar
+router.post('/', createScholar);
+
+// DELETE a scholar
+router.delete('/:id', deleteScholar);
+
+// PATCH (Update) a scholar
+router.patch('/:id', updateScholar);
+
+module.exports = router;
