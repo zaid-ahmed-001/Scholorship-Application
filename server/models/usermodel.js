c855bf638462fd59442c0785d1f530d6ef4818ae
Shashank-Mishra-1 <shashankmishra2604@gmail.com>
2023-10-05 01:09:13 +0530

    update user data

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -3,15 +3,67 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    name :{
+    FirstName :{
         type : String,
         required : true
     },   
-    mobile :{
-        type : Number,
+    LastName :{
+        type : String,
+        required : true
+    },   
+    FatherName :{
+        type : String,
+        required : true
+    }, 
+    PassWord :{
+        type : String,
+        required : true
+    },
+    ConfirmWord :{
+        type : String,
         required : true
     },
     email :{
+        type : String,
+        required : true
+    },
+    ContactNumber :{
+        type : String,
+        required : true
+    },
+    DateofBirth:{
+        type : String,
+        required : true
+    },
+    Enrollno :{
+        type : String,
+        required : true
+    },
+    Gender :{
+        type : String,
+        required : true
+    },
+    religion :{
+        type : String,
+        required : true
+    },
+    MaritialStatus :{
+        type : String,
+        required : true
+    },
+    SpouseName :{
+        type : String,
+        required : true
+    },
+    Disabled :{
+        type : String,
+        required : true
+    },
+    Orphan :{
+        type : String,
+        required : true
+    },
+    Aggrement :{
         type : String,
         required : true
     }
