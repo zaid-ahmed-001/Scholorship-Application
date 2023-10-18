bfe04f775396dfd60502b1603a5e1ae0bece491a
Fubuki <klick4278@gmail.com>
2023-10-19 00:06:11 +0530

    backend fix

--- server/http.js
+++ server/http.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require("cors");
+const connectDB = require("./config/db");
 
-dotenv.config();
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+<<<<<<< Updated upstream
 // app.use(cors())
 
 // connectDB();
@@ -22,14 +25,28 @@ const port = process.env.PORT || 4000;
 //     credentials: true,
 //   })
 // );
+=======
+connectDB();
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000/"
+    ],
+    credentials: true,
+  })
+);
+>>>>>>> Stashed changes
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 const db = mongoose.connection;
 
